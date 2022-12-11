b2f85282280c145686e107e39ebe062fadec6a51
Erica Zhang <ericazhang@MacBook-Pro-326.local>
2022-12-10 20:02:59 -0800

    change

--- source/Data Application/app_server.R
+++ source/Data Application/app_server.R
@@ -5,33 +5,46 @@ library("ggplot2")
 library("plotly")
 library("reshape2")
 
+co2_emissions <- read.csv("owid-co2-data.csv")
+co2_emissions_by_year <- co2_emissions %>%
+  group_by(year) %>%
+  summarise(
+    co2 = sum(co2, na.rm = TRUE),
+    co2_growth_abs = sum(co2_growth_abs, na.rm = TRUE),
+    co2_growth_prct = sum(co2_growth_prct, na.rm = TRUE),
+    co2_including_luc = sum(co2_including_luc, na.rm = TRUE),
+    co2_including_luc_growth_abs = sum(co2_including_luc_growth_abs, na.rm = TRUE),
+    co2_including_luc_growth_prct = sum(co2_including_luc_growth_prct, na.rm = TRUE)
+  )
+year_range <- range(co2_emissions_by_year$year)
+co2_2000 <- co2_emissions_by_year %>% 
+  filter(year == "2000") %>% 
+  pull(co2)
+
+co2_growth_abs_2000 <- co2_emissions_by_year %>% 
+  filter(year == "2000") %>% 
+  pull(co2_growth_abs)
+
+co2_growth_prct_2000 <- co2_emissions_by_year %>% 
+  filter(year == "2000") %>% 
+  pull(co2_growth_prct)
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  incarceration <- read.csv("owid-co2-data.csv")
-  incarceration_by_year <- incarceration %>%
-    group_by(year) %>%
-    summarise(
-      co2 = sum(co2, na.rm = TRUE),
-      co2_growth_abs = sum(co2_growth_abs, na.rm = TRUE),
-      co2_growth_prct = sum(co2_growth_prct, na.rm = TRUE),
-      co2_including_luc = sum(co2_including_luc, na.rm = TRUE),
-      co2_including_luc_growth_abs = sum(co2_including_luc_growth_abs, na.rm = TRUE),
-      co2_including_luc_growth_prct = sum(co2_including_luc_growth_prct, na.rm = TRUE)
-    )
-  year_range <- range(incarceration_by_year$year)
   
   output$line_chart <- renderPlotly({
-    line_data <- incarceration_by_year %>%
+    line_data <- co2_emissions_by_year %>%
       filter(year > input$year_range[1], year < input$year_range[2]) %>%
       select(year, input$data_type) %>%
-      melt(id.vars = "year")
+      melt(id.vars = "year", measure.vars = input$data_type)
 
     line_chart <- ggplot(line_data) +
       aes(x = year, y = value, color = variable) + 
       geom_line() + 
-      labs(x = "Year", y = "CO₂ data Type") +
+      labs(x = "Year", y = "CO₂ data Type", 
+           title = "Various Types of CO₂ Emissions Data with Resepct to Specific Year Range") + 
       theme_bw() +
-      theme(rect = element_rect(fill = "transparent"))
+      theme(rect = element_rect(fill = "transparent")) 
 
     return(line_chart)
   })
--- source/Data Application/app_ui.R
+++ source/Data Application/app_ui.R
@@ -8,6 +8,26 @@ source("app_server.R")
 
 intro_page <- tabPanel(
   "Introduction",
+  h1("Exploring Six Types of CO₂ Emission Data", style = " font-size: 40px ; font-family: 'times'; font-si16pt"),
+  img("", src = "https://media.istockphoto.com/id/167231386/photo/detail-of-white-smoke-polluted-sky.jpg?s=612x612&w=0&k=20&c=hExCnY1CN7xieBUcBTQ8h37TDLkSWCT06l8bbShbQvE=",
+      style = "display: block; margin-left: auto; margin-right: auto;", height = 600, width = 1400),
+  br(),
+  p(paste0("As climate change can literally affect anyone on this planet, it is significantly
+    important for us to get more aware of it. And CO₂ emission trend is a big part of it.
+    This application focused specifically on 6 types of CO₂ emission to explore,
+    in order to provide an analysis and visualization of the trend of CO₂ emission globally.
+    In detail, the variables of CO₂ included are the annual total production-basd of
+    CO₂ emission(excluding land-use change), the annual growth and the annual percentage
+    growth of it. As well as the annual total production-basd of CO₂ emission that
+    included land-use change, and the annual growth and annual percentage growth of it.
+    These 6 variables can reveal a general trend of CO₂ emission and also explore how the
+    factor of land-use change can play a role in CO₂ emission. To first get a sense of the
+    CO₂ emission, I would like to introduce the three relevant values of CO₂ for the year 2000.
+    In the year 2000, the total CO₂ emission(excluding land-use change) was ", co2_2000,
+    "tonnes, the annual growth was ", co2_growth_abs_2000, "tonnes, and the annual percentage growth was ", co2_growth_prct_2000,
+    "%. "),
+    style = "font-family: 'times'; font-si16pt ; font-size:17px;"
+  )
 )
 
 year_input <- sliderInput(
@@ -15,7 +35,7 @@ year_input <- sliderInput(
   label = "Select Year",
   min = year_range[1],
   max = year_range[2],
-  value = year_range, 
+  value = year_range,
   width = "75%"
 )
 
@@ -37,10 +57,19 @@ select_input <- selectInput(
 
 interactive_page <- tabPanel(
   "Interative Visualization",
+  h1("Visualizing the Trends of Different CO₂ Data Type", style = " font-size: 40px ; font-family: 'times'; font-si16pt"),
+  br(),
   setSliderColor("#515A5A", 1),
-  year_input, 
-  select_input, 
-  plotlyOutput(outputId = "line_chart")
+  year_input,
+  select_input,
+  plotlyOutput(outputId = "line_chart"),
+  p("This chart was included to visualize the trend of 6 different CO₂ emission data types 
+    with respect to a specific year range. The user could easily get a clear sense of the 
+    circumstance of  CO₂ emission during a particular year range they are interested in. 
+    From this chart, we could see that CO₂ emission grew dramatically starting the year 1850, 
+    and land-use change played a big role during the years 1850 to 1950. Besides, 
+    variables of growth and percentage growth also started to have a lot of float starting 1850. "
+    , style = "font-family: 'times'; font-si16pt ; font-size:17px;"),
 )
 
 ui <- navbarPage(
